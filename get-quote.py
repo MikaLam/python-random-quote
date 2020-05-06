a0d15ff863c0450b34232020262495d0638927e5
unknown <guanming.lam@gmail.com>
2020-05-06 13:39:15 +0800

    Print out more than one quote at a time

--- get-quote.py
+++ get-quote.py
@@ -10,7 +10,13 @@ def primary():
   last = len(quotes) - 1
   rnd = random.randint(0, last)
 
-  print(quotes[rnd], end='')
+  # Print more than 1 quote  
+  rnd2 = random.randint(0, last)
+  print(quotes[rnd])
+  print(quotes[rnd2])
+
+  # Print without extra line  
+  # print(quotes[rnd], end='')
 
 if __name__== "__main__":
   primary()
